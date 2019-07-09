bac3254bcd0c527d269a23c52b3187b98241f796
tytyne <45592250+tytyne@users.noreply.github.com>
2019-07-09 16:40:07 +0100

    push on heroku

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ app.use('/signup/users', users);
 app.use('/login/auth', auth);
 let port = 5000;
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port  ' + port);
 });
