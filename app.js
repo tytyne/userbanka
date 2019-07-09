89b45e56caf8a13ec0d7a163f98a3582552b2fdd
tytyne <dusaflora2@gmail.com>
2019-07-09 16:55:30 +0100

    add changes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(express.json());
 app.use('/signup/users', users);
 app.use('/login/auth', auth);
-let port = 5000;
+let port =process.env.PORT|| 5000;
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
